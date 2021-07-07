01f069e29733615c0de35d370c9216b23dbf3457
Milinko <milinko.m.dragovic@gmail.com>
2021-07-07 11:48:47 +0200

    Fix config rollup - compile scss

--- dist/mdui.esm.js
+++ dist/mdui.esm.js
@@ -1,79 +1,38 @@
-import { defineComponent, pushScopeId, popScopeId, openBlock, createBlock, createVNode, createTextVNode, toDisplayString, withScopeId } from 'vue';
+import { defineComponent, openBlock, createBlock } from 'vue';
 
-var script = /*#__PURE__*/defineComponent({
-  name: 'MduiSample',
-
-  // vue component name
-  data() {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null
-      }
-    };
+var script = defineComponent({
+  name: "MdInputText",
+  props: {
+    modelValue: {
+      type: String,
+      default: ""
+    }
   },
-
-  computed: {
-    changedBy() {
-      const {
-        message
-      } = this;
-      if (!message.action) return 'initialized';
-      return `${message.action} ${message.amount || ''}`.trim();
+  methods: {
+    onInputChange(event) {
+      const target = event.target;
+      this.$emit("update:modelValue", target.value);
     }
 
   },
-  methods: {
-    increment(arg) {
-      const amount = typeof arg !== 'number' ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
-    },
-
-    decrement(arg) {
-      const amount = typeof arg !== 'number' ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-
-    reset() {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
+  computed: {
+    isInputFilled() {
+      return this.modelValue !== null && this.modelValue.toString().length > 0;
     }
 
   }
 });
 
-const _withId = /*#__PURE__*/withScopeId("data-v-2d1160ba");
-
-pushScopeId("data-v-2d1160ba");
-
-const _hoisted_1 = {
-  class: "mdui-sample"
-};
-
-const _hoisted_2 = /*#__PURE__*/createTextVNode(".");
-
-popScopeId();
-
-const render = /*#__PURE__*/_withId((_ctx, _cache, $props, $setup, $data, $options) => {
-  return openBlock(), createBlock("div", _hoisted_1, [createVNode("p", null, [createTextVNode("The counter was " + toDisplayString(_ctx.changedBy) + " to ", 1), createVNode("b", null, toDisplayString(_ctx.counter), 1), _hoisted_2]), createVNode("button", {
-    onClick: _cache[1] || (_cache[1] = (...args) => _ctx.increment && _ctx.increment(...args))
-  }, " Click +1 "), createVNode("button", {
-    onClick: _cache[2] || (_cache[2] = (...args) => _ctx.decrement && _ctx.decrement(...args))
-  }, " Click -1 "), createVNode("button", {
-    onClick: _cache[3] || (_cache[3] = $event => _ctx.increment(5))
-  }, " Click +5 "), createVNode("button", {
-    onClick: _cache[4] || (_cache[4] = $event => _ctx.decrement(5))
-  }, " Click -5 "), createVNode("button", {
-    onClick: _cache[5] || (_cache[5] = (...args) => _ctx.reset && _ctx.reset(...args))
-  }, " Reset ")]);
-});
+function render(_ctx, _cache) {
+  return openBlock(), createBlock("input", {
+    type: "text",
+    class: ['md-input-text md-component', {
+      'md-filled': _ctx.isInputFilled
+    }],
+    value: _ctx.modelValue,
+    onInput: _cache[1] || (_cache[1] = (...args) => _ctx.onInputChange && _ctx.onInputChange(...args))
+  }, null, 42, ["value"]);
+}
 
 function styleInject(css, ref) {
   if ( ref === void 0 ) ref = {};
@@ -102,17 +61,16 @@ function styleInject(css, ref) {
   }
 }
 
-var css_248z = "\n.mdui-sample[data-v-2d1160ba] {\n    display: block;\n    width: 400px;\n    margin: 25px auto;\n    border: 1px solid #ccc;\n    background: #eaeaea;\n    text-align: center;\n    padding: 25px;\n}\n.mdui-sample p[data-v-2d1160ba] {\n    margin: 0 0 1em;\n}\n";
+var css_248z = ".md-input-text {\n  margin: 0;\n  padding: 0.5rem 0.5rem;\n  border-radius: 0.25rem;\n  font-size: 1rem;\n  color: #495057;\n  background: #ffffff;\n  border: 1px solid #ced4da;\n  transition: background-color 0.2s, color 0.2s, border-color 0.2s, box-shadow 0.2s;\n  appearance: none;\n}\n.md-input-text:focus {\n  outline: 0 none;\n  outline-offset: 0;\n  box-shadow: 0 0 0 0.2rem #a6d5fa;\n  border-color: #2196F3;\n}\n.md-fluid .md-input-text {\n  width: 100%;\n}\n\n/* InputGroup */\n.md-input-group {\n  display: flex;\n  align-items: stretch;\n  width: 100%;\n}\n.md-input-group-addon {\n  display: flex;\n  align-items: center;\n  justify-content: center;\n}\n.md-input-group .md-float-label {\n  display: flex;\n  align-items: stretch;\n  width: 100%;\n}\n.md-input-group .md-input-text,\n.md-fluid .md-input-group .md-input-text,\n.md-input-group .md-inputwrapper,\n.md-fluid .md-input-group .md-input {\n  flex: 1 1 auto;\n  width: 1%;\n}\n\n/* Floating Label */\n.md-float-label {\n  display: block;\n  position: relative;\n}\n.md-float-label label {\n  position: absolute;\n  pointer-events: none;\n  top: 50%;\n  margin-top: -0.5rem;\n  transition-property: all;\n  transition-timing-function: ease;\n  line-height: 1;\n}\n.md-float-label textarea ~ label {\n  top: 1rem;\n}\n.md-float-label input:focus ~ label,\n.md-float-label input.md-filled ~ label,\n.md-float-label textarea:focus ~ label,\n.md-float-label textarea.md-filled ~ label,\n.md-float-label .md-inputwrapper-focus ~ label,\n.md-float-label .md-inputwrapper-filled ~ label {\n  top: -0.75rem;\n  font-size: 12px;\n}\n.md-float-label .input:-webkit-autofill ~ label {\n  top: -20px;\n  font-size: 12px;\n}\n.md-input-icon-left,\n.md-input-icon-right {\n  position: relative;\n  display: inline-block;\n}\n.md-input-icon-left > i,\n.md-input-icon-right > i {\n  position: absolute;\n  top: 50%;\n  margin-top: -0.5rem;\n}\n.md-fluid .md-input-icon-left,\n.md-fluid .md-input-icon-right {\n  display: block;\n  width: 100%;\n}\n.md-float-label > label {\n  left: 0.5rem;\n  color: #6c757d;\n  transition-duration: 0.2s;\n}";
 styleInject(css_248z);
 
 script.render = render;
-script.__scopeId = "data-v-2d1160ba";
 
 /* eslint-disable import/prefer-default-export */
 
 var components = /*#__PURE__*/Object.freeze({
-  __proto__: null,
-  MduiSample: script
+    __proto__: null,
+    MdInputText: script
 });
 
 // Import vue components
@@ -124,4 +82,4 @@ const install = function installMdui(app) {
 }; // Create module definition for Vue.use()
 
 export default install;
-export { script as MduiSample };
+export { script as MdInputText };
--- dist/mdui.ssr.js
+++ dist/mdui.ssr.js
@@ -55,82 +55,37 @@ function _arrayLikeToArray(arr, len) {
 
 function _nonIterableRest() {
   throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
-}var script = /*#__PURE__*/vue.defineComponent({
-  name: 'MduiSample',
-  // vue component name
-  data: function data() {
-    return {
-      counter: 5,
-      initCounter: 5,
-      message: {
-        action: null,
-        amount: null
-      }
-    };
-  },
-  computed: {
-    changedBy: function changedBy() {
-      var _ref = this,
-          message = _ref.message;
-
-      if (!message.action) return 'initialized';
-      return "".concat(message.action, " ").concat(message.amount || '').trim();
+}var script = vue.defineComponent({
+  name: "MdInputText",
+  props: {
+    modelValue: {
+      type: String,
+      default: ""
     }
   },
   methods: {
-    increment: function increment(arg) {
-      var amount = typeof arg !== 'number' ? 1 : arg;
-      this.counter += amount;
-      this.message.action = 'incremented by';
-      this.message.amount = amount;
-    },
-    decrement: function decrement(arg) {
-      var amount = typeof arg !== 'number' ? 1 : arg;
-      this.counter -= amount;
-      this.message.action = 'decremented by';
-      this.message.amount = amount;
-    },
-    reset: function reset() {
-      this.counter = this.initCounter;
-      this.message.action = 'reset';
-      this.message.amount = null;
+    onInputChange: function onInputChange(event) {
+      var target = event.target;
+      this.$emit("update:modelValue", target.value);
+    }
+  },
+  computed: {
+    isInputFilled: function isInputFilled() {
+      return this.modelValue !== null && this.modelValue.toString().length > 0;
     }
   }
-});var _withId = /*#__PURE__*/vue.withScopeId("data-v-2d1160ba");
-
-vue.pushScopeId("data-v-2d1160ba");
-
-var _hoisted_1 = {
-  class: "mdui-sample"
-};
-
-var _hoisted_2 = /*#__PURE__*/vue.createTextVNode(".");
-
-vue.popScopeId();
-
-var render = /*#__PURE__*/_withId(function (_ctx, _cache, $props, $setup, $data, $options) {
-  return vue.openBlock(), vue.createBlock("div", _hoisted_1, [vue.createVNode("p", null, [vue.createTextVNode("The counter was " + vue.toDisplayString(_ctx.changedBy) + " to ", 1), vue.createVNode("b", null, vue.toDisplayString(_ctx.counter), 1), _hoisted_2]), vue.createVNode("button", {
-    onClick: _cache[1] || (_cache[1] = function () {
-      return _ctx.increment && _ctx.increment.apply(_ctx, arguments);
+});function render(_ctx, _cache) {
+  return vue.openBlock(), vue.createBlock("input", {
+    type: "text",
+    class: ['md-input-text md-component', {
+      'md-filled': _ctx.isInputFilled
+    }],
+    value: _ctx.modelValue,
+    onInput: _cache[1] || (_cache[1] = function () {
+      return _ctx.onInputChange && _ctx.onInputChange.apply(_ctx, arguments);
     })
-  }, " Click +1 "), vue.createVNode("button", {
-    onClick: _cache[2] || (_cache[2] = function () {
-      return _ctx.decrement && _ctx.decrement.apply(_ctx, arguments);
-    })
-  }, " Click -1 "), vue.createVNode("button", {
-    onClick: _cache[3] || (_cache[3] = function ($event) {
-      return _ctx.increment(5);
-    })
-  }, " Click +5 "), vue.createVNode("button", {
-    onClick: _cache[4] || (_cache[4] = function ($event) {
-      return _ctx.decrement(5);
-    })
-  }, " Click -5 "), vue.createVNode("button", {
-    onClick: _cache[5] || (_cache[5] = function () {
-      return _ctx.reset && _ctx.reset.apply(_ctx, arguments);
-    })
-  }, " Reset ")]);
-});function styleInject(css, ref) {
+  }, null, 42, ["value"]);
+}function styleInject(css, ref) {
   if ( ref === void 0 ) ref = {};
   var insertAt = ref.insertAt;
 
@@ -155,9 +110,8 @@ var render = /*#__PURE__*/_withId(function (_ctx, _cache, $props, $setup, $data,
   } else {
     style.appendChild(document.createTextNode(css));
   }
-}var css_248z = "\n.mdui-sample[data-v-2d1160ba] {\n    display: block;\n    width: 400px;\n    margin: 25px auto;\n    border: 1px solid #ccc;\n    background: #eaeaea;\n    text-align: center;\n    padding: 25px;\n}\n.mdui-sample p[data-v-2d1160ba] {\n    margin: 0 0 1em;\n}\n";
-styleInject(css_248z);script.render = render;
-script.__scopeId = "data-v-2d1160ba";/* eslint-disable import/prefer-default-export */var components$1=/*#__PURE__*/Object.freeze({__proto__:null,MduiSample: script});var install = function installMdui(app) {
+}var css_248z = ".md-input-text {\n  margin: 0;\n  padding: 0.5rem 0.5rem;\n  border-radius: 0.25rem;\n  font-size: 1rem;\n  color: #495057;\n  background: #ffffff;\n  border: 1px solid #ced4da;\n  transition: background-color 0.2s, color 0.2s, border-color 0.2s, box-shadow 0.2s;\n  appearance: none;\n}\n.md-input-text:focus {\n  outline: 0 none;\n  outline-offset: 0;\n  box-shadow: 0 0 0 0.2rem #a6d5fa;\n  border-color: #2196F3;\n}\n.md-fluid .md-input-text {\n  width: 100%;\n}\n\n/* InputGroup */\n.md-input-group {\n  display: flex;\n  align-items: stretch;\n  width: 100%;\n}\n.md-input-group-addon {\n  display: flex;\n  align-items: center;\n  justify-content: center;\n}\n.md-input-group .md-float-label {\n  display: flex;\n  align-items: stretch;\n  width: 100%;\n}\n.md-input-group .md-input-text,\n.md-fluid .md-input-group .md-input-text,\n.md-input-group .md-inputwrapper,\n.md-fluid .md-input-group .md-input {\n  flex: 1 1 auto;\n  width: 1%;\n}\n\n/* Floating Label */\n.md-float-label {\n  display: block;\n  position: relative;\n}\n.md-float-label label {\n  position: absolute;\n  pointer-events: none;\n  top: 50%;\n  margin-top: -0.5rem;\n  transition-property: all;\n  transition-timing-function: ease;\n  line-height: 1;\n}\n.md-float-label textarea ~ label {\n  top: 1rem;\n}\n.md-float-label input:focus ~ label,\n.md-float-label input.md-filled ~ label,\n.md-float-label textarea:focus ~ label,\n.md-float-label textarea.md-filled ~ label,\n.md-float-label .md-inputwrapper-focus ~ label,\n.md-float-label .md-inputwrapper-filled ~ label {\n  top: -0.75rem;\n  font-size: 12px;\n}\n.md-float-label .input:-webkit-autofill ~ label {\n  top: -20px;\n  font-size: 12px;\n}\n.md-input-icon-left,\n.md-input-icon-right {\n  position: relative;\n  display: inline-block;\n}\n.md-input-icon-left > i,\n.md-input-icon-right > i {\n  position: absolute;\n  top: 50%;\n  margin-top: -0.5rem;\n}\n.md-fluid .md-input-icon-left,\n.md-fluid .md-input-icon-right {\n  display: block;\n  width: 100%;\n}\n.md-float-label > label {\n  left: 0.5rem;\n  color: #6c757d;\n  transition-duration: 0.2s;\n}";
+styleInject(css_248z);script.render = render;/* eslint-disable import/prefer-default-export */var components$1=/*#__PURE__*/Object.freeze({__proto__:null,MdInputText: script});var install = function installMdui(app) {
   Object.entries(components$1).forEach(function (_ref) {
     var _ref2 = _slicedToArray(_ref, 2),
         componentName = _ref2[0],
@@ -166,7 +120,7 @@ script.__scopeId = "data-v-2d1160ba";/* eslint-disable import/prefer-default-exp
     app.component(componentName, component);
   });
 }; // Create module definition for Vue.use()
-var components=/*#__PURE__*/Object.freeze({__proto__:null,'default': install,MduiSample: script});// only expose one global var, with component exports exposed as properties of
+var components=/*#__PURE__*/Object.freeze({__proto__:null,'default': install,MdInputText: script});// only expose one global var, with component exports exposed as properties of
 // that global var (eg. plugin.component)
 
 Object.entries(components).forEach(function (_ref) {
